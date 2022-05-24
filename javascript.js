e139707979f82fb04f36f2169d0597e952d97b5c
idbic <dbicga@gmail.com>
2022-05-24 18:00:17 -0500

    updated the frame rate and functionality of the game. Now I will work on detecting collisions

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ let hero;
 let corn; 
 let redbull;
 let anudaCorn;
+let thirdCorn;
 
 
 let game = document.querySelector('#game')
@@ -21,7 +22,7 @@ class Hero {
         this.height = height; 
         this.width = width; 
         this.alive = true; 
-        this.speed = 20;
+        this.speed = 3;
     }
 // added this.speed and jergins move function to this class
     render(){
@@ -41,15 +42,16 @@ class Hero {
 // used codealong to get some js going. I know I will have to modify this but I wanted to start
 
 window.addEventListener("DOMContentLoaded", function (e){
-    hero = new Hero(325, 750, "#870a66", 60, 90);
+    hero = new Hero(325, 750, "#870a66", 30, 90);
     corn = new Hero(null, 10, "#ffff00", 250, 75);
-    anudaCorn = new Hero(350, 10, "#ffff00", 250, 75)
+    anudaCorn = new Hero(350, 10, "#ffff00", 5, 75);
+    thirdCorn = new Hero(150, 10, "#ffff00", 250, 75);
     
-    
-    const runGame = setInterval(gameLoop, 120);
+    const runGame = setInterval(gameLoop, 8);
 }) 
 
 function gameLoop(){
+    
     ctx.clearRect(0, 0, game.width, game.height);
 
     // if (shrek.alive){
@@ -60,10 +62,13 @@ function gameLoop(){
     hero.render();
     corn.render();
     corn.move();
-    anudaCorn.render();
+    anudaCorn.render()
     anudaCorn.move();
+    thirdCorn.render();
+    thirdCorn.move()
     reCornGen();
     reAnudaCornGen();
+    thirdCornRegen();
     
 }
 //added the corn move method/function 
@@ -79,10 +84,10 @@ function movementHandler(e){
             hero.y < (game.height - hero.height) ? hero.y += 50 : null;
             break
         case "ArrowLeft":
-            hero.x > 0 ? hero.x -= 50 : null;
+            hero.x > 0 ? hero.x -= 40 : null;
             break
         case "ArrowRight":
-            hero.x < (game.width - hero.width) ? hero.x += 50 : null;
+            hero.x < (game.width - hero.width) ? hero.x += 40 : null;
             break
         // case "mousemove":
         //     hero.x < (game.width - hero.width) ? hero.x += 10 : null;
@@ -104,25 +109,36 @@ game.height = 850
 //render falling objects using modifying lecture function
 //added a conditional to joels newshrek (im back on my bs)
 function reCornGen() {
-    if(corn.y === 850){
+    if(corn.y === 900){
     corn.alive = false;
     setTimeout(function(){
-        let x = Math.floor(Math.random() * game.width) - 40;
-        let y = 10
-        corn = new Hero(x, y, "#ffff00", 250, 75)
+        let x = Math.floor(Math.random() * game.width) - 200;
+        let y = 0
+        corn = new Hero(x, y, "#ffff00", 225, 75)
     }, 1000)
     return true;}
 
 }
 
 function reAnudaCornGen() {
-    if(corn.y === 850) {
+    if(anudaCorn.y === 900) {
         anudaCorn.alive = false;
     setTimeout(function(){
-        let x = Math.floor(Math.random() * game.width) - 40;
-        let y = 10
-        anudaCorn = new Hero(x, y, "#ffff00", 250, 75)
-    }, 1000)
+        let x = Math.floor(Math.random() * game.width) - 200;
+        let y = 0
+        anudaCorn = new Hero(x, y, "#ffff00", 200, 75)
+    }, 2000)
     return true;}
 
+}
+
+function thirdCornRegen(){
+    if(thirdCorn.y === 900) {
+        thirdCorn.alive = false;
+    setTimeout(function(){
+        let x = Math.floor(Math.random() * game.width) - 200;
+        let y = 0
+        thirdCorn = new Hero(x, y, "#ffff00", 175, 75)
+    }, 1000)
+    return true;}
 }
